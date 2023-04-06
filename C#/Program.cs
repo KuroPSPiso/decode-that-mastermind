98d9d6368b7e0111fdff7c2895121c6ad9a52ea4
Ryan van den Bogaard <bogaardryan@hotmail.com>
2023-04-06 07:25:07 +0200

    Rem func
    removed unused, added rem feature

--- C#/Program.cs
+++ C#/Program.cs
@@ -6,15 +6,16 @@ namespace calc
 {
 	public class Program
 	{
-		static List<string> basePossibilityList = new List<string>();
-		static List<string> possibilitiesList = new List<string>();
-
 		static Dictionary<string, int> searchPile = new Dictionary<string, int>();
 		static Dictionary<string, int> discardPile = new Dictionary<string, int>();
 		static Dictionary<string, int> potentialPile = new Dictionary<string, int>();
 
 		static readonly int[] BaseX = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; //0-9
-		static int[] X1 = new int[10], X2 = new int[10], X3 = new int[10], X4 = new int[10], X5 = new int[10]; //5 numbers, used for rebuild:  -1 = false, 0 = neutral, 1 = true
+		/// <summary>
+		/// 5 numbers [x1][x2][x3][x4][x5] (exmpl. #12345 x1=1, x2=2 ...etc).
+		/// Used for rebuild:  -1 = false, 0 = neutral
+		/// </summary>
+		static int[] X1 = new int[10], X2 = new int[10], X3 = new int[10], X4 = new int[10], X5 = new int[10]; 
 
 		static int[] confirmedDigits = {
 			-1, -1, -1, -1, -1
@@ -22,9 +23,20 @@ namespace calc
 
 		static string expectedResult = "00000";
 		static bool debug = true;
+		static bool debugNumber = true;
+		static bool isRemove = false;
 
 		static void checkKnown(string newEntry, int count)
         {
+			if(isRemove)
+            {
+				searchPile.Remove(newEntry);
+				discardPile.Remove(newEntry);
+				potentialPile.Remove(newEntry);
+				RebuildComparitor();
+				return;
+            }
+
 			if (!searchPile.ContainsKey(newEntry)) searchPile.Add(newEntry, count); //add entered word to list with the correct count.
 			else return; //already processed
 
@@ -35,7 +47,7 @@ namespace calc
 			}
 			else
             {
-				//if
+				//TODO: IF
 				potentialPile.Add(newEntry, count);
 			}
 
@@ -74,7 +86,25 @@ namespace calc
 					X5[x5] = -1;
 				}
 			}
+			confirmedDigits[0] = CheckRemainder(X1);
+			confirmedDigits[1] = CheckRemainder(X2);
+			confirmedDigits[2] = CheckRemainder(X3);
+			confirmedDigits[3] = CheckRemainder(X4);
+			confirmedDigits[4] = CheckRemainder(X5);
 		}
+
+		static int CheckRemainder(int[] xNUM)
+        {
+			int remainder = 0;
+			int checkVal = 0;
+			for(int i = 0; i < xNUM.Length; i++)
+            {
+				if (xNUM[i] != -1) checkVal = i;
+				remainder = remainder - xNUM[i];
+            }
+
+			return remainder == 9 ? checkVal : -1;
+        }
 
 		static void RenderNumbers()
         {
@@ -99,30 +129,15 @@ namespace calc
 			Console.WriteLine(sb.ToString());
         }
 
-		static void RemoveDuds()
-        {
-			//because of future remove feature, recalc all
-			possibilitiesList = new List<string>(basePossibilityList);
-			
-
-			for (int i = 0; i < 99999; i++)
-			{
-
-				basePossibilityList.Add(i.ToString().PadLeft(5, '0'));
-			}
-		}
-
 		public static void Main()
 		{
 			Random rand = new Random();
 			int randomNumber = rand.Next(0, 99999);
+
+			if (debugNumber) randomNumber = 11111;
+
 			expectedResult = randomNumber.ToString().PadLeft(5, '0');
 			
-			for (int i = 0; i < 99999; i++)
-			{
-				basePossibilityList.Add(i.ToString().PadLeft(5, '0'));
-			}
-
 			BaseX.CopyTo(X1, 0);
 			BaseX.CopyTo(X2, 0);
 			BaseX.CopyTo(X3, 0);
@@ -139,6 +154,7 @@ namespace calc
 				string data = Console.ReadLine();
 				if (data.Length < 5) goto redo;
 				int dataInt = 0;
+				if (isRemove = data.StartsWith('r')) data = data.Remove(0, 1);
 				if (!Int32.TryParse(data, out dataInt)) goto redo;
 
 				int countCorrect = 0;
@@ -152,6 +168,13 @@ namespace calc
 
 				RenderNumbers();
 				Console.WriteLine("No. correct in ({1}): {0}", d, data);
+				Console.WriteLine("confirmed ({1}{1}{1}{1}{1})",
+					confirmedDigits[0] != -1 ? confirmedDigits[0].ToString() : '?',
+					confirmedDigits[1] != -1 ? confirmedDigits[1].ToString() : '?',
+					confirmedDigits[2] != -1 ? confirmedDigits[2].ToString() : '?',
+					confirmedDigits[3] != -1 ? confirmedDigits[3].ToString() : '?',
+					confirmedDigits[4] != -1 ? confirmedDigits[4].ToString() : '?'
+					);
 
 				if (countCorrect < 5)
 				{
